9504dae9747e3b39be4fc1b0da57b6ce2b61b7d6
Unknown <antonio958@hotmail.com>
2017-08-01 10:54:31 +0200

    Sin ref

---
+++ PasoPorReferencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasoPorReferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            Write(a);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+
+        static void Write(int a) {
+            a = 20;
+        }
+    }
+}
